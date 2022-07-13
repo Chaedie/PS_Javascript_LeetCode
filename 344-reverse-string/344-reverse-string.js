f772fc01b9c94171a65c0c03227bc9ee28ebc25d
ChaedongIm <chaedong.im.dev@gmail.com>
2022-07-13 19:43:20 +0900

    Time: 140 ms (47.92%), Space: 48.8 MB (94.19%) - LeetHub

--- 344-reverse-string/344-reverse-string.js
+++ 344-reverse-string/344-reverse-string.js
@@ -3,7 +3,5 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-  for (let i = 0; i < Math.floor(s.length / 2); i++) {
-    [s[i], s[(s.length - 1) - i]] = [s[(s.length - 1) - i], s[i]];
-  }
+  s.reverse();
 };
