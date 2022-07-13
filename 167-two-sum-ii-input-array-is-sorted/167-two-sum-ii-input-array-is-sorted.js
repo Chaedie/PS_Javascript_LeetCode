a7a6134d3b9b5917f2af9137eca7112380237ee8
ChaedongIm <chaedong.im.dev@gmail.com>
2022-07-13 18:53:18 +0900

    Time: 105 ms (32.01%), Space: 43.3 MB (20.70%) - LeetHub

---
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+  let lt = 0;
+  let rt = numbers.length -1;
+  let twoSum = 0;
+  while (lt < rt) {
+    twoSum = numbers[lt] + numbers[rt];
+    if (twoSum === target) {
+      return [lt + 1, rt + 1];
+    }
+    if (twoSum < target) {
+      lt++;
+      continue;
+    }
+    if (twoSum > target) {
+      rt--;
+      continue;
+    }
+  }
+  
+};
